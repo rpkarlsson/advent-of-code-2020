b3810dbf78a1bcd683307fc43577bcb6bed99322
Robin Karlsson <robinpkarlsson@gmail.com>
2020-12-11 20:00:39 +0100

    Day 10 part 1

---
+++ src/rpkarlsson/day_10.clj
@@ -0,0 +1,34 @@
+(ns rpkarlsson.day-10
+  (:require [rpkarlsson.util.file :as file]))
+
+(defn find-connectable
+  [rating adapters]
+  (->> adapters
+       (map-indexed (fn [index x] [index x]))
+       (filter (fn [[_ x]] (<= (- x 3) rating x)))))
+
+(defn drop-nth [n coll]
+  (concat
+   (take n coll)
+   (drop (inc n) coll)))
+
+(defn find-differences
+  [adapters]
+  (loop [adapters' (conj (into [] (sort adapters)) (+ (apply max adapters) 3))
+         target 0
+         differences []]
+    (cond
+      (empty? adapters') differences
+      :else
+      (if-let [[index value] (first (find-connectable target adapters'))]
+        (recur (drop-nth index adapters')
+               value
+               (conj differences (- value target)))
+        differences))))
+
+(defn part-1
+  []
+  (let [freq (->> (file/xf-day-lines 10 (map #(Integer/parseInt %)))
+                  (find-differences)
+                  (frequencies))]
+    (* (get freq 1) (get freq 3))))
