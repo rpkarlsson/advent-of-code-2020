608cca9c4832347f4b20f8150c3441a6ec970111
Robin Karlsson <robinpkarlsson@gmail.com>
2020-12-13 17:49:25 +0100

    Day 13 part 1

---
+++ src/rpkarlsson/day_13.clj
@@ -0,0 +1,27 @@
+(ns rpkarlsson.day-13
+  (:require [clojure.string :as str]
+            [rpkarlsson.util.file :as file]))
+
+(defn closest-to-target
+  [target sum next]
+  (if (<= target sum)
+    (reduced sum)
+    (+ sum next)))
+
+(defn id+wait+departure
+  [interval target]
+  (let [departure (reduce (partial closest-to-target target) 0 (repeat interval))]
+    [interval (- departure target) departure]))
+
+(defn part-1
+  []
+  (let [target 1005595]
+    (->> (file/xf-day-lines 13 (map #(str/split % #",")))
+         (first)
+         (remove #{"x"})
+         (map #(Integer/parseInt %))
+         (map #(id+wait+departure % target))
+         (sort-by second)
+         (first)
+         (take 2)
+         (apply *))))
