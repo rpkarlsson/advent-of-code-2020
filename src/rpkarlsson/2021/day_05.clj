02457de3980cb6dcd3ab15b48734dd28ce885c88
Robin Karlsson <robin.karlsson@fortnox.se>
2021-12-05 07:30:39 +0100

    Day 5 part 1

---
+++ src/rpkarlsson/2021/day_05.clj
@@ -0,0 +1,56 @@
+(ns rpkarlsson.2021.day-05
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def sample
+  "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+")
+
+(def parse-lines
+  (comp (map #(str/split % #" -> "))
+        (map (partial map #(str/split % #",")))))
+
+(def parsed-sample
+  (->> (str/split sample #"\n")
+       (into [] parse-lines)))
+
+(defn expand-line
+  [[[x1 y1] [x2 y2]]]
+  (let [x1int (Integer/parseInt x1)
+        x2int (Integer/parseInt x2)
+        xlength (Math/abs (- x2int x1int))
+        xstart (Math/min x1int x2int)
+        y1int (Integer/parseInt y1)
+        y2int (Integer/parseInt y2)
+        ylength (Math/abs (- y2int y1int))
+        ystart (Math/min y1int y2int)]
+    (when (or (= x1int x2int)
+              (= y1int y2int))
+      (when-not (and (zero? xlength)
+                     (zero? ylength))
+        (map (fn [x y] [x y])
+             (if (zero? xlength)
+               (repeat x1int)
+               (range xstart (+ 1 xstart xlength)))
+             (if (zero? ylength)
+               (repeat y1int)
+               (range ystart (+ 1 ystart ylength))))))))
+
+(defn part-1
+  []
+  (with-open [f (io/reader (io/resource "2021/day_05.txt"))]
+    (->> (into [] parse-lines (line-seq f))
+         (mapcat expand-line)
+         (frequencies)
+         (map second)
+         (filter #(<= 2 %))
+         (count))))
