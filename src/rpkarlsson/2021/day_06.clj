82cff4945a4729078c48b0a40474164d74239f52
Robin Karlsson <robin.karlsson@fortnox.se>
2021-12-06 09:09:04 +0100

    Day 6 part 2

--- src/rpkarlsson/2021/day_06.clj
+++ src/rpkarlsson/2021/day_06.clj
@@ -13,22 +13,32 @@
   (if (= days 0)
     state
     (recur
-     (->> state
-          (mapcat (fn [n]
-                    (if (zero? n)
-                      [birthing-rate (+ birthing-rate new-fish-birthing-rate)]
-                      [(dec n)]))))
+     {0 (get state 1 0)
+      1 (get state 2 0)
+      2 (get state 3 0)
+      3 (get state 4 0)
+      4 (get state 5 0)
+      5 (get state 6 0)
+      6 (+ (get state 0 0) (get state 7 0) )
+      7 (get state 8 0)
+      8 (get state 0 0)}
      (dec days))))
 
-(defn part-1
-  []
-(let [input (->> (str/split (slurp (io/resource "2021/day_06.txt")) #",")
+(defn run
+  [days]
+  (let [input (->> (str/split (slurp (io/resource "2021/day_06.txt")) #",")
                    (map str/trim)
-                   (map #(Integer/parseInt %)))]
-    (->> (run-loop input 80)
-         (count))))
+                   (map #(Integer/parseInt %))
+                   (frequencies))]
+    (->> (run-loop input days)
+         (map second)
+         (reduce +))))
+
+(defn part-1 [] (run 80))
+(defn part-2 [] (run 256))
 
 (comment
   (part-1)
+  (part-2)
 
   ,)
