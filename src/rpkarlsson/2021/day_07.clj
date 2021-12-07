17da6a9607a984b85b1491e85e2226c23be2eef7
Robin Karlsson <robin.karlsson@fortnox.se>
2021-12-07 13:48:14 +0100

    Day 7 part 2

--- src/rpkarlsson/2021/day_07.clj
+++ src/rpkarlsson/2021/day_07.clj
@@ -10,10 +10,29 @@
    (map #(str/trim %))
    (map #(Integer/parseInt %))))
 
-(let [data (into [] input-xf [sample #_(slurp (io/resource "2021/day_07.txt"))])
-      mean (nth (sort data) (/ (int (count data)) 2))]
-  (->> data
-       (map #(- % mean))
-       (map #(Math/abs %))
-       (map (fn [n] (reduce + (range (inc n)))))
-       (reduce +)))
+(defn part-1
+  []
+  (let [data (into [] input-xf [#_sample (slurp (io/resource "2021/day_07.txt"))])
+        mean (nth (sort data) (/ (int (count data)) 2))]
+    (->> data
+         (map #(- % mean))
+         (map #(Math/abs %))
+         (reduce +))))
+
+(defn part-2
+  []
+  (let [data (into [] input-xf [#_sample (slurp (io/resource "2021/day_07.txt"))])]
+    (apply min
+           (->> (range (apply min data) (apply max data))
+                (map (fn [pos]
+                       (->> data
+                            (map #(- % pos))
+                            (map #(Math/abs %))
+                            (map #(+ % (reduce + (range %))))
+                            (reduce +))))))))
+
+(comment
+  (part-1)
+  (part-2)
+
+  ,)
