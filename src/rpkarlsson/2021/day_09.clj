a6875309223364a111a75d2cc60ffb7677380426
Robin Karlsson <robin.karlsson@fortnox.se>
2021-12-09 21:20:34 +0100

    Day 9 part 2

--- src/rpkarlsson/2021/day_09.clj
+++ src/rpkarlsson/2021/day_09.clj
@@ -2,7 +2,6 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-
 (def sample
   "2199943210
 3987894921
@@ -15,31 +14,76 @@
   [[(dec x) y]  [(inc x) y]
    [x (dec y)]  [x (inc y)]])
 
-(defn collect-low-points
+(defn get-coord
+  [grid [x y]]
+  (nth (nth grid x nil) y nil))
+
+(defn collect-low-point-coords
   [grid coords]
   (->> coords
        (reduce (fn [found next-coord]
               (let [neighbours (->> (neighbouring-coords next-coord)
-                                    (map #(nth (nth grid (first %) []) (second %) nil))
-                                    (remove nil?)
-                                    )
-                    next-coord-value (nth (nth grid (first next-coord)) (second next-coord))]
+                                    (map #(get-coord grid %))
+                                    (remove nil?))
+                    next-coord-value (get-coord grid next-coord)]
                 (if (every? #(< next-coord-value %) neighbours)
-                  (conj found next-coord-value)
+                  (conj found next-coord)
                   found)))
             [])))
 
-(def part-1
+(def grid
+  (with-open [r (io/reader (io/resource "2021/day_09.txt"))]
+    (->> #_(str/split sample #"\n")
+         (line-seq r)
+         (map (partial map str))
+         (mapv (partial map #(Integer/parseInt %))))))
+
+(defn all-coords
+  [grid]
+  (for [x (range (count grid))
+        y (range (count (first grid)))]
+    [x y]))
+
+(defn part-1
   []
-  (let [grid (with-open [r (io/reader (io/resource "2021/day_09.txt"))]
-               (->> #_(str/split sample #"\n")
-                    (line-seq r)
-                    (map (partial map str))
-                    (mapv (partial map #(Integer/parseInt %)))))
-        coords (for [x (range (count grid))
-                     y (range (count (first grid)))]
-                 [x y])]
-    (->> coords
-         (collect-low-points grid)
-         (map inc)
-         (reduce +))))
+  (->> (all-coords grid)
+       (collect-low-point-coords grid)
+       (map #(get-coord grid %))
+       (map inc)
+       (reduce +)))
+
+(defn find-neighbours [grid low-points]
+  (->> low-points
+       (map (fn [point]
+              (loop [found-points #{}
+                     coords-to-check [point]]
+                (if (empty? coords-to-check)
+                  found-points
+                  (let [next-coord (first coords-to-check)
+                        point-value (get-coord grid next-coord)]
+                    (if (or (nil? point-value)
+                            (= 9 point-value)
+                            (contains? found-points next-coord))
+                      (recur found-points (rest coords-to-check))
+                      (recur (conj found-points next-coord)
+                             (rest
+                              (concat
+                               (into [] coords-to-check)
+                               (neighbouring-coords next-coord))))))))))))
+
+
+(defn part-2 []
+  (->> (all-coords grid)
+       (collect-low-point-coords grid)
+       (find-neighbours grid)
+       (sort-by count)
+       (reverse)
+       (take 3)
+       (map count)
+       (reduce *)))
+
+(comment
+  (part-1)
+  (part-2)
+
+  ,)
