cd56ce399dc997453dfd38250b97e2ca72a82701
Robin Karlsson <robin.karlsson@fortnox.se>
2021-12-10 08:40:42 +0100

    Day 10 part 2

--- src/rpkarlsson/2021/day_10.clj
+++ src/rpkarlsson/2021/day_10.clj
@@ -20,13 +20,11 @@
    \[ \]
    \< \>})
 
-
 (def char->point
   {\) 3
    \] 57
    \} 1197
    \> 25137})
-
 
 (def closing (-> start->end vals set))
 
@@ -39,27 +37,70 @@
      ;; (map (partial map identity) (str/split sample #"\n"))
      ;; (take 1)
 
-(->> input
-     (map (fn [line]
-            (loop [current-line line
-                   queue '()
-                   correct []]
-              (let [[current] current-line
-                    expected (get start->end (first queue))]
-                (cond
-                  (empty? current-line)
-                  nil
+(defn part-1
+  []
+  (->> input
+       (map (fn [line]
+              (loop [current-line line
+                     queue '()
+                     correct []]
+                (let [[current] current-line
+                      expected (get start->end (first queue))]
+                  (cond
+                    (empty? current-line)
+                    nil
 
-                  (= expected current)
-                  (recur (rest current-line) (pop queue) (conj correct current))
+                    (= expected current)
+                    (recur (rest current-line) (pop queue) (conj correct current))
 
-                  (and (contains? closing current)
-                       (not= current (peek queue)))
-                  current
+                    (and (contains? closing current)
+                         (not= current (peek queue)))
+                    current
 
-                  :else
-                  (recur (rest current-line) (conj queue current) (conj correct current)))))))
+                    :else
+                    (recur (rest current-line) (conj queue current) (conj correct current)))))))
 
-     (remove nil?)
-     (map char->point)
-     (reduce +))
+       (remove nil?)
+       (map char->point)
+       (reduce +)))
+
+(def char->part-2-point
+  {\) 1
+   \] 2
+   \} 3
+   \> 4})
+
+(defn part-2
+  []
+  (let [points (->> input
+                    (map (fn [line]
+                           (loop [current-line line
+                                  queue '()
+                                  correct []]
+                             (let [[current] current-line
+                                   expected (get start->end (first queue))]
+                               (cond
+                                 (empty? current-line)
+                                 queue
+
+                                 (= expected current)
+                                 (recur (rest current-line) (pop queue) (conj correct current))
+
+                                 (and (contains? closing current)
+                                      (not= current (peek queue)))
+                                 nil
+
+                                 :else
+                                 (recur (rest current-line) (conj queue current) (conj correct current)))))))
+                    (remove nil?)
+                    (remove empty?)
+                    (map (partial map start->end))
+                    (map (partial reduce (fn [sum char] (+ (get char->part-2-point char) (* sum 5)) ) 0))
+                    (sort))]
+    (nth points (/ (count points) 2))))
+
+(comment
+  (part-1)
+  (part-2)
+
+  ,)
