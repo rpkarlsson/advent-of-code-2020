c43fcccd30780f889aa34fb6b2a5372fc1dc8b62
Robin Karlsson <robin.karlsson@fortnox.se>
2022-12-01 19:26:27 +0100

    2022 day 1

---
+++ src/rpkarlsson/2022/day_01.clj
@@ -0,0 +1,48 @@
+(ns rpkarlsson.2022.day-01
+  (:require [clojure.string :as str]))
+
+(def sample
+  "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000")
+
+(defn mmap
+  [f xs]
+  (map #(map f %) xs))
+
+(defn sum-by-elf
+  [s]
+  (->> (str/split s #"\n\n")
+       (map str/split-lines)
+       (mmap #(Integer/parseInt %))
+       (map #(reduce + %))
+       (map-indexed vector)
+       (map (juxt (comp inc first) second))
+       (sort-by second)
+       (reverse)))
+
+#_(ffirst (sum-by-elf sample))
+
+(defn part-1
+  []
+  (first (sum-by-elf (slurp "resources/2022/day_01.txt"))))
+
+(defn part -2
+  []
+  (->> "resources/2022/day_01.txt"
+       slurp
+       sum-by-elf
+       (take 3)
+       (map second)
+       (reduce +)))
