6efe777465209d6ae1f7a47b2fe832990116ff51
Robin Karlsson <robin.karlsson@fortnox.se>
2022-12-10 12:41:19 +0100

    2022 day 10 part 2

--- src/rpkarlsson/2022/day_10.clj
+++ src/rpkarlsson/2022/day_10.clj
@@ -1,158 +1,5 @@
 (ns rpkarlsson.2022.day-10
-  (:require [clojure.string :as str]
-            [rpkarlsson.utils :as u]))
-
-(def operation->cycles
-  {:addx 2
-   :noop 1})
-
-(def sample
-  "addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop")
+  (:require [clojure.string :as str]))
 
 (def cycles
   #{20, 60, 100, 140, 180, 220})
@@ -160,29 +7,57 @@ noop")
 (defn calculate-signal-strength
   [xs cycle register]
   (if (contains? cycles cycle)
-    (do (println cycle register)
-      (conj xs (* cycle register)))
+    (conj xs (* cycle register))
     xs))
 
-(defn solve
+(defn draw [screen register cycle]
+  (if (<= (dec register) (mod (dec cycle) 40) (inc register))
+    (assoc-in screen [(dec cycle)] "#")
+    screen))
+
+(defn run
   [{:keys [register cycle signal-strengths] :as state} [op step]]
-  (let [cycle' (inc cycle)
-        register' (if (= :noop op)
+  (let [register' (if (= :noop op)
                     register
                     (+ register step))]
     (-> state
-        (assoc :cycle cycle'
+        (assoc :cycle (inc cycle)
                :register register'
-               :signal-strengths (calculate-signal-strength signal-strengths cycle' register)))))
+               :signal-strengths (calculate-signal-strength signal-strengths cycle register))
+        (update :screen draw register cycle))))
 
-(->> sample
-     (str/split-lines)
-     (map #(str/split % #" "))
-     (map (juxt (comp keyword first)
-                (comp (fnil parse-long "0") second)))
-     (reduce (fn [coll [op _ :as step]]
-               (if (not= op :noop)
-                 (conj coll [:noop] step)
-                 (conj coll step)))
-             [])
-     (reduce solve {:cycle 1 :register 1 :signal-strengths []}))
+(defn solve [input]
+  (->> input
+       (str/split-lines)
+       (map #(str/split % #" "))
+       (map (juxt (comp keyword first)
+                  (comp (fnil parse-long "0") second)))
+       (reduce (fn [coll [op _ :as step]]
+                 (if (not= op :noop)
+                   (conj coll [:noop] step)
+                   (conj coll step)))
+               [])
+       (reduce run {:cycle 1
+                    :register 1
+                    :signal-strengths []
+                    :screen (->> (range 0 240)
+                                 (map (constantly "."))
+                                 vec)})))
+
+(defn part-1
+  []
+  (->> #_sample
+       (-> "resources/2022/day_10.txt" slurp)
+       (solve)
+       :signal-strengths
+       (reduce +)))
+
+(defn part-2
+  []
+  (->> #_sample
+       (-> "resources/2022/day_10.txt" slurp)
+       (solve)
+       (:screen)
+       (partition 40)
+       (map str/join)
+       (map println)))
