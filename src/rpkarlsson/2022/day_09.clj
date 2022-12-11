be640b2eece1ad5843cb997e92caca13d0a951bf
Robin Karlsson <robin.karlsson@fortnox.se>
2022-12-11 13:25:19 +0100

    2022 day 9 part 2
    Struggled with this one.
    Couldn't get the follow function to properly handle all movements.
    Had to get unstuck and use the Clojurians slack.
    Also picked up some neat tricks:
    - reductions
    - model movements as changes to a coord
    - improved adjacency check
    - use `re-seq` to make parsing a bit easier

--- src/rpkarlsson/2022/day_09.clj
+++ src/rpkarlsson/2022/day_09.clj
@@ -1,6 +1,7 @@
 (ns rpkarlsson.2022.day-09
-  (:require [clojure.string :as str]
-            [rpkarlsson.utils :as u]))
+  (:require
+   [rpkarlsson.utils :as u]
+   [clojure.math :as math]))
 
 (def sample
   "R 4
@@ -12,52 +13,97 @@ D 1
 L 5
 R 2")
 
+(def direction->change
+  {\U [-1 0]
+   \D [1 0]
+   \L [0 -1]
+   \R [0 1]})
 
-(defn ->directions [[direction times]]
-  (take times (repeat direction)))
+(defn adjacent?
+  [[x1 y1] [x2  y2]]
+  (and (<= (abs (- x1 x2)) 1)
+       (<= (abs (- y1 y2)) 1)))
 
-(defn parse [s]
+(defn parse
+  [s]
   (->> s
-       str/split-lines
-       (map #(str/split % #" "))
-       (map (juxt first (comp parse-long second)))
-       (mapcat ->directions)))
+       (re-seq #"(L|R|U|D) (\d+)" )
+       (map (juxt (comp first second) (comp parse-long #(nth % 2))))))
 
+(defn expand
+  [[direction steps]]
+  (repeat steps direction))
+
+(def dg (u/make-grid 5 6 \.))
+
+(defn print-grid
+  [things]
+  (println "\n------------\n")
+  (doseq [l (->>  (reduce (fn [g thing]
+                            (assoc-in g thing "#"))
+                          dg
+                          (last things)))]
+    (println l)))
 
 (defn move
-  [[x y] move]
-  (case move
-    "R" [x (inc y)]
-    "L" [x (dec y)]
-    "U" [(dec x) y]
-    "D" [(inc x) y]))
+  [coord [dx dy]]
+  (-> coord
+      (update 0 + dx)
+      (update 1 + dy)))
 
-(defn adjacent? [[x y] tail]
-  (let [n (for [x' (range (dec x) (+ x 2))
-                y' (range (dec y) (+ y 2))]
-            [x' y'])]
-    (contains? (set n) tail)))
+(defn follow
+  [coll follower]
+  (let [leader (last coll)]
+    (if (adjacent? follower leader)
+      (conj coll follower)
+      (let [dx (- (first leader) (first follower))
+            dy (- (second leader) (second follower))]
+        (conj coll
+              [(long (+ (first follower) (math/signum dx)))
+               (long (+ (second follower) (math/signum dy)))])))))
 
-(defn in-row-or-col
-  [[x y] [a b]]
-  (or (= x a) (= y b)))
+(defn run-steps [acc step]
+  (let [[head & tail] (last acc)
+        head' (move head (get direction->change step))
+        rope (->> tail
+                  (reduce follow [head']))]
+    (conj acc rope)))
 
-(defn run [input]
-  (loop [head [5 0]
-         tail [5 0]
-         visited #{[5 0]}
-         moves (parse input)]
-    (if (empty? moves)
-      visited
-      (let [head' (move head (first moves))
-            tail' (cond
-                    (adjacent? head' tail) tail
-                    (in-row-or-col head tail) (move tail (first moves))
-                    :else head)]
-        (recur head'
-               tail'
-               (conj visited tail')
-               (rest moves))))))
+(defn solve
+  [initial-state]
+  (->> "resources/2022/day_09.txt"
+       slurp
+       parse
+       (mapcat expand)
+       (reductions run-steps initial-state)
+       last
+       (map last)
+       (into #{})
+       (count)))
 
-#_(-> sample run count)
-#_(->> "resources/2022/day_09.txt" slurp run count)
+(defn solve-1
+  []
+  (solve [(vec (repeat 2 [4 0]))]))
+
+(def sample-2
+  "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20")
+
+(defn solve-2
+  []
+  (solve [(vec (repeat 10 [4 0]))]))
+
+
+(comment
+  ;; Print each step
+  (do (println "\n---------- START ---------------\n")
+      (doseq [t (reductions run-steps [[[4 0] [4 0]]] (->> sample parse (mapcat expand)))]
+        (print-grid t)))
+
+  ,)
--- src/rpkarlsson/utils.clj
+++ src/rpkarlsson/utils.clj
@@ -6,3 +6,19 @@
    (map (partial map f)))
   ([f coll]
    (map (partial map f) coll)))
+
+(defn generate-coords-in-grid
+  "Given a grid of size `x` and `y` generates all coords in that grid."
+  [x y]
+  (->> (for [x' (range 0 x)
+            y' (range 0 y)]
+        [x' y'])
+       (map vec)
+       vec))
+
+(defn make-grid [rows columns value]
+  (->> (range (* rows columns))
+       (map (constantly value))
+       (partition columns)
+       (map vec)
+       vec))
